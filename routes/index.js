0deca1e4691d205051454bd763d6ac766461ba5c
DEVgotti <david.lmonras@gmail.com>
2024-02-27 00:05:25 +0000

    add placeholder info

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Playstation 5', type: 'Se Busca', price: '200€', tags: ['gaming', 'console'], image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2023/07/sony-playstation-5-1689016881412-3080326.jpg' });
 });
 
 module.exports = router;
