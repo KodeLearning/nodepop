dfd8c4c3c162caaf06e1034cef8bd2309a6bab27
DEVgotti <david.lmonras@gmail.com>
2024-03-02 21:54:23 +0000

    Update index to show all products

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,35 @@
 var express = require('express')
 var router = express.Router()
+const productController = require('../controllers/products')
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  const data = await fetch('http://192.168.1.129:3000/api/products?start=0&limit=3')
-  const products = await data.json()
-  res.render('index', { products: products.results })
+  try {
+    // Filtros
+    const filter = {}
+    const filterByName = req.query.name
+    const filterByTag = req.query.tag
+    // Paginación
+    const start = req.query.start
+    const limit = req.query.limit
+    // Ordenación
+    const sort = req.query.sort
+    // Field selection
+    const fields = req.query.fields
+
+    if (filterByName) {
+      filter.name = filterByName
+    }
+
+    if (filterByTag) {
+      filter.tag = filterByTag
+    }
+
+    const products = await productController.allProducts(filter, start, limit, sort, fields)
+    res.render('index', { products })
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
